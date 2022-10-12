ffc2e90f4a602de6cf55562b704c827efc80d086
mikeomatic9000 <mike@mjeffers.com>
2022-10-12 16:54:46 -0700

    Add QuantConnect scripts

--- CoinbasePro/Services/Products/Types/CandleGranularity.cs
+++ CoinbasePro/Services/Products/Types/CandleGranularity.cs
@@ -6,7 +6,10 @@
         Minutes5 = 300,
         Minutes15 = 900,
         Hour1 = 3600,
+        Hour4 = 14400,
         Hour6 = 21600,
-        Hour24 = 86400
+        Hour24 = 86400,
+        Day1 = 86400,
+        Day3 = 259200
     }
 }
